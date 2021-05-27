4a1471cb84d0d729a608dc78d915b282a4be1e55
SkylarACD <71157379+SkylarACD@users.noreply.github.com>
2021-05-27 10:19:43 -0500

    format iframe

--- app_virus.py
+++ app_virus.py
@@ -383,7 +383,7 @@ app.layout = html.Div([
                     html.Div(
                         className="col-lg",
                         children=[
-                        html.Div(
+                            html.Div(
                                 className="card mb-3",
                                 children=[
                                     html.Div(
@@ -402,31 +402,59 @@ app.layout = html.Div([
     ),
 #########
     
-    html.Div([
-        html.Div([
-            dcc.Graph(
-                id='example-graph',
+    html.Div(
+        className="container",
+        children=[
+            html.Div(
+                className="row",
+                children=[
+                    html.Div(
+                        className="col-sm",
+                        children=[
+                            html.Div(
+                                className="card mb-3",
+                                children=[
+                                    html.Div(
+                                        className="card-body",
+                                        children=[
+                                            dcc.Graph(
+                                                id='example-graph',
+                                                style={"height": "450px%", "width": "100%"},
                 #hoverData={'points': [{'customdata': 'Japan'}]}
+                                            )
+                                        ]
+                                    )
+                                ]
+                            )
+                        ]
+                    ),
+                    html.Div(
+                        className="col-sm",
+                        children=[
+                            html.Div(
+                                className="card mb-0",
+                                children=[
+                                    html.Div(
+                                        className="card-body",
+                                        children=[                
+                                            html.Iframe(
+                                                className="centered",
+                                                style={"height": "450px", "width": "100%"},
+                                                src="assets/out3.html",
+                                            )
+                                        ]
+                                    )
+                                ]
+                            )
+                        ]
+                    ),
+                ]
             )
-        ], style={'width': '49%', 'display': 'inline-block', 'padding': '0 20'}),
+        ]
+    ),
         
-    #######
-        html.Div([
-            html.Iframe(
-                src="assets/out3.html",
-                #style={"top": "right", "height": "50px"}
-            )
-            ### maybe render full graph later 
-            #dcc.Graph(
-            #    id='example-graph2',
-                #hoverData={'points': [{'customdata': 'Japan'}]}
-            #)
-        ], style={'width': '49%', 'display': 'inline-block', 'padding': '0 20'}),
-    ], style={
-        'borderBottom': 'thin lightgrey solid',
-        'backgroundColor': 'rgb(250, 250, 250)',
-        'padding': '10px 5px'
-    }),
+                
+
 
     html.Div([
 
